8f944ab0909e03bdc52e6d2df59b5a2d6b686721
davemflick <davemflick@yahoo.com>
2020-05-24 18:46:59 -0400

    Reorder files

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(ing => {
-    if (props.ingredients[ing] > 0) {
-      return <li key={ing}><span style={{textTransform: "capitalize"}}>{ing}</span>: {props.ingredients[ing]}</li>;
-    }
-    return null;
-  });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.totalPrice}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" click={props.cancel}>CANCEL</Button>
-      <Button btnType="Success" click={props.continue}>CONTINUE</Button>
-    </Aux>
-  );
+class OrderSummary extends Component {
+  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(ing => {
+      if (this.props.ingredients[ing] > 0) {
+        return <li key={ing}><span style={{textTransform: "capitalize"}}>{ing}</span>: {this.props.ingredients[ing]}</li>;
+      }
+      return null;
+    });
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.totalPrice}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" click={this.props.cancel}>CANCEL</Button>
+        <Button btnType="Success" click={this.props.continue}>CONTINUE</Button>
+      </Aux>
+    );
+  }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './SideDrawer.module.css';
 
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Logo from '../../Logo/Logo';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItems from '../NavigationItems/NavigationItems';
