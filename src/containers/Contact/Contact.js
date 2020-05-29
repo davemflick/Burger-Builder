1383747011ce91f32b834e4375691efb49702270
davemflick <davemflick@yahoo.com>
2020-05-28 21:18:56 -0400

    Form validation

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -3,39 +3,124 @@ import Button from '../../components/UI/Button/Button';
 import classes from './Contact.module.css';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import Input from '../../components/UI/Input/Input';
 
 class Contact extends Component {
   state = {
-    contact: {
-      name: '',
-      email: '',
-      address: {
-        street: '',
-        zipCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Email'
+        },
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5,
+        },
+        valid: false,
+        touched: false,
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'},
+          ]
+        },
+        value: 'fastest',
+        validation: {},
+        valid: true,
+      }
     },
+    formIsValid: false,
     loading: false,
   }
 
-  onInputChange = (e) => {
-    const contact = {...this.state.contact};
-    const field = String(e.target.getAttribute('name'));
-    if (contact[field] || contact[field] === '') {
-      contact[field] = e.target.value;
-    } else {
-      contact.address[field] = e.target.value;
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
     }
-    this.setState({contact: contact});
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
+  onInputChange = (input, e) => {
+    const form = {...this.state.orderForm};
+    // Clone Deep
+    const formEl = { ...form[input] }
+    formEl.value = e.target.value;
+    formEl.valid = this.checkValidity(formEl.value, formEl.validation);
+    formEl.touched = true;
+    form[input] = formEl;
+
+    let formIsValid = true;
+    for (let inputIdentifier in form) {
+      formIsValid = form[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({orderForm: form, formIsValid: formIsValid});
   }
 
   orderHandler = (e) => {
     e.preventDefault();
+    if (!this.state.formIsValid) {
+      return;
+    }
     this.setState({loading: true});
+    const formData = {};
+    Object.keys(this.state.orderForm).forEach(input => {formData[input] = this.state.orderForm[input].value;});
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer: this.state.contact,
-      deliveryMethod: 'fastest'
+      orderData: formData,
     }
 
     axios.post('/orders.json', order)
@@ -50,13 +135,24 @@ class Contact extends Component {
   }
 
   render() {
+    const inputs = Object.keys(this.state.orderForm).map(input => {
+      const details = {...this.state.orderForm[input]}
+      return <Input
+        key={input}
+        elementtype={details.elementType}
+        elementconfig={details.elementConfig}
+        value={details.value}
+        invalid={!details.valid}
+        shouldValidate={details.validation}
+        touched={details.touched}
+        change={(e) => this.onInputChange(input, e)}
+        />
+    });
+
     let form = (
-      <form>
-        <input type="text" name="name" placeholder="Your name" value={this.state.contact.name} onChange={this.onInputChange} />
-        <input type="email" name="email" placeholder="Your email" value={this.state.contact.email} onChange={this.onInputChange}  />
-        <input type="text" name="street" placeholder="Your street" value={this.state.contact.address.street} onChange={this.onInputChange}  />
-        <input type="text" name="zipCode" placeholder="Your Zip Code" value={this.state.contact.address.zipCode} onChange={this.onInputChange}  />
-        <Button btnType="Success" click={this.orderHandler}>ORDER</Button>
+      <form onSubmit={this.orderHandler}>
+        {inputs}
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
     if (this.state.loading) {
